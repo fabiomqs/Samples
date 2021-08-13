6334f24a4d37dd05a0c182ffbe04e49a8d3b686f
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-13 15:34:24 -0300

    sending exemplo to github

---
+++ src/test/java/net/guides/springboot2/springboot2postgresqljpahibernatecrudexample/Springboot2PostgresqlJpaHibernateCrudExampleApplicationTests.java
@@ -0,0 +1,8 @@
+package net.guides.springboot2.springboot2postgresqljpahibernatecrudexample;
+
+class Springboot2PostgresqlJpaHibernateCrudExampleApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
