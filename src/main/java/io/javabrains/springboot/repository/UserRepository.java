c55721c7b00301804258b72b41e1927fd3b0ff9e
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-13 15:26:54 -0300

    sending exemplo to github

---
+++ src/main/java/io/javabrains/springboot/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package io.javabrains.springboot.repository;
+
+import io.javabrains.springboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
