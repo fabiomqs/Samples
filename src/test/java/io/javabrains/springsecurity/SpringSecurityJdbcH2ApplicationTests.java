58292345723444bb6b7f17860223115bded9c801
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-13 15:23:13 -0300

    sending exemplo to github

---
+++ src/test/java/io/javabrains/springsecurity/SpringSecurityJdbcH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springsecurity;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SpringSecurityJdbcH2ApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
