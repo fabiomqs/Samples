309325702e18ed824abfc522dcb34c8e4450e1e3
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-15 10:45:17 -0300

    generated files

---
+++ src/test/java/guru/springgramework/spring5jokes/Spring5JokesAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springgramework.spring5jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
