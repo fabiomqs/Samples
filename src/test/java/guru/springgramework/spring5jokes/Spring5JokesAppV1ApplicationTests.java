10affdcb979b314bce31d96bac4d11b515e41da2
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-15 11:47:12 -0300

    Done. Closes #1.

--- src/test/java/guru/springgramework/spring5jokes/Spring5JokesAppV1ApplicationTests.java
+++ src/test/java/guru/springgramework/spring5jokes/Spring5JokesAppV1ApplicationTests.java
@@ -3,10 +3,10 @@ package guru.springgramework.spring5jokes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Spring5JokesAppV1ApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
