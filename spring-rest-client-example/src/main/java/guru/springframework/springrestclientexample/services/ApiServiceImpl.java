4c302e86d8aeb2ced090f9c15ddf071aea215f01
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-08-29 15:39:05 -0300

    URI Components Builder #4

--- spring-rest-client-example/src/main/java/guru/springframework/springrestclientexample/services/ApiServiceImpl.java
+++ spring-rest-client-example/src/main/java/guru/springframework/springrestclientexample/services/ApiServiceImpl.java
@@ -1,24 +1,35 @@
 package guru.springframework.springrestclientexample.services;
 
 import guru.springframework.api.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ApiServiceImpl implements ApiService {
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+    private final String api_url;
+    private final String USERS_SERVICE = "users";
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api_url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers() {
-        User[] users = restTemplate.getForObject("https://fakestoreapi.com/users", User[].class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .pathSegment(USERS_SERVICE);
+
+        User[] users = restTemplate.getForObject(uriBuilder.toUriString(), User[].class);
 
         return Arrays.asList(users);
     }
