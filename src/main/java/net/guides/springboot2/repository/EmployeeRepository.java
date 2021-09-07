6f5b2a28c6ab62654dee12db9e7e88bd3b4a364a
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-07 15:11:15 -0300

    prepare to move

--- src/main/java/net/guides/springboot2/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package net.guides.springboot2.repository;
-
-import net.guides.springboot2.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
